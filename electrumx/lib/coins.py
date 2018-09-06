dcd0130b815631f7636044f11d72e98818546160
josephnicholas <josephnicholasalcantara@gmail.com>
2018-09-07 01:11:05 +0800

    Add feature

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -3,7 +3,7 @@
 #
 # All rights reserved.
 #
-# The MIT License (MIT)
+# The MIT License ()
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
